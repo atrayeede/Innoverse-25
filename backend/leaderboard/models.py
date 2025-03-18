a054fb16d887e99b84260bd93fee381f2d55f819
ASHISH PATEL <pashishp005@gmail.com>
2025-03-19 00:50:52 +0530

    Save local changes before merging remote updates

--- backend/leaderboard/models.py
+++ backend/leaderboard/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Leaderboard(models.Model):
     name = models.CharField(max_length=100)
-    time = models.FloatField()  # Time taken for 10 questions
-    questions = models.JSONField()  # Store questions & answers as JSON
+    time = models.FloatField()  # Time taken for 10 questions (lower is better)
+    questions = models.JSONField(default=list)  # Default to an empty list
 
     def __str__(self):
         return f"{self.name} - {self.time}s"
